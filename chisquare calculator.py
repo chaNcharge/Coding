a0a0fe084d11e4b38af9a8ab919954f37a994ce6
chaNcharge <chancharge@gmail.com>
2017-10-12 09:45:03 -0700

    Add raw inputs to chisquare calculator

--- chisquare calculator.py
+++ chisquare calculator.py
@@ -2,4 +2,11 @@ from scipy.stats import chisquare
 
 o = []
 
+while True:
+    tocalc = raw_input("Enter a number. Enter a blank to finish. ")
+    if tocalc == "":
+        break
+    else:
+        o.append(int(tocalc))
+
 print(chisquare(o))
