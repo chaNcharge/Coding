01d5c901be03b78be8f51389beb56e25f88c319a
chaNcharge <chancharge@gmail.com>
2018-01-15 00:47:32 -0800

    Check if there's duplicate numbers in lottery.py

--- lottery.py
+++ lottery.py
@@ -1,11 +1,26 @@
+#!/usr/bin/env python3
+''' 
+A lottery that chooses 5 random numbers from 0 - 9.
+5 numbers are user inputs.
+If they all match, the user wins.
+'''
+
 from random import sample
 
 lotterynumbers = sample(range(10), 5)
 
 my_numbers = []
+numsinlist = 0
 
-for _ in range(5):
-    my_numbers.append(int(input("Enter 1 number, then press return: ")))
+# Checks if there are less than 5 numbers in the list. Does not add duplicates.
+while numsinlist < 5:
+    numinput = int(input("Enter 1 number 0 - 9, then press return: "))
+    if numinput in my_numbers:
+        print("That number is already in the ticket.")
+    else:
+        my_numbers.append(numinput)
+        numsinlist += 1
+
 print("Lottery numbers are " + str(lotterynumbers))
 print("Your numbers are " + str(my_numbers))
 if set(my_numbers) == set(lotterynumbers):
