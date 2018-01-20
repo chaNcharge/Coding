a2c3112c4915a7267a3e5e72dec6c079a3838694
chaNcharge <chancharge@gmail.com>
2018-01-19 17:20:48 -0800

    Stop being an idiot, rewrite renamenumber.py

--- renamenumber.py
+++ renamenumber.py
@@ -1,10 +1,13 @@
 import os
 
-find = input("Find: ")
+choice = input("Remove the start or end of file? Input 'start' or 'end': ")
+characters = int(input("Characters to remove: "))
+files = os.listdir(".")
+files.remove("renamenumber.py")
 
-for fname in os.listdir("."):
+for fname in files:
     fname_noext, ext = os.path.splitext(fname)
-    if fname.startswith(find):
-        os.rename(fname, fname[len(find):])
-    if fname_noext.endswith(find):
-        os.rename(fname, (fname_noext[:len(find)] + ext))
+    if choice == "start":
+        os.rename(fname, fname[characters:])
+    if choice == "end":
+        os.rename(fname, (fname_noext[:-characters] + ext))
