3c64bec11a1f9b388bcf2de2594c5cbf7f33ca08
chaNcharge <chancharge@gmail.com>
2018-01-21 18:42:41 -0800

    Fix wrong code in wrong place

--- renamereplace.py
+++ renamereplace.py
@@ -3,7 +3,6 @@ import os
 search = input("Search for: ")
 replace = input("Replace with: ")
 path = os.path.dirname(os.path.realpath(__file__))
-path.remove("renamereplace.py")
 
 for f in os.listdir(path):
     os.rename(os.path.join(path, f), 
