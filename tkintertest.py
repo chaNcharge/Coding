32164452247728133ab80aefc9f89d363a70770b
chaNcharge <chancharge@gmail.com>
2018-01-26 09:46:53 -0800

    Add checkbutton and combobox

--- tkintertest.py
+++ tkintertest.py
@@ -1,22 +1,37 @@
-import tkinter as ttk
+import tkinter as tk
+import tkinter.ttk as ttk
  
-window = ttk.Tk() 
+window = tk.Tk() 
 window.title("chaNcharge's app")
-window.geometry('350x200')
+window.geometry('500x300')
 
-lbl = ttk.Label(window, text="Hello there!")
-lbl.grid(column=0, row=0)
+lbl = tk.Label(window, text="You suck!")
+lbl.grid(column=1, row=0)
 
-txt = ttk.Entry(window, width=10)
-txt.grid(column=1, row=0)
+txt = tk.Entry(window, width=10)
+txt.grid(column=2, row=0)
 txt.focus()
-# Left off at 5. Add a combobox widget
+
+combo = ttk.Combobox(window)
+combo['values']= (1, 2, 3, 4, 5, "many")
+combo.current(0)
+combo.grid(column=0, row=0)
+
+chk_state = tk.BooleanVar()
+chk_state.set(False)
+chk = ttk.Checkbutton(window, text="Chosen?", var=chk_state)
+chk.grid(column=0, row=1)
+# Left off at 7 Add radio buttons widgets
 
 def clicked():
-    res = "I am " + txt.get()
+    res = "I am " + str(combo.get()) + " " + txt.get()
     lbl.configure(text= res)
+def close():
+    window.destroy()
 
-btn = ttk.Button(window, text="Click Me", command=clicked)
-btn.grid(column=2, row=0)
+btn = tk.Button(window, text="Click Me", command=clicked)
+btn.grid(column=3, row=0)
+closebtn = tk.Button(window, text="Close", command=close)
+closebtn.grid(column=1, row=1)
  
 window.mainloop()
