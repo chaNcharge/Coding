41c4ddd9669385625245b7501a9606763a583da6
chaNcharge <chancharge@gmail.com>
2018-02-28 23:31:30 -0800

    Add app tests, get music working.

--- pyqt5-music-player.py
+++ pyqt5-music-player.py
@@ -1,11 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QMenuBar, QAction
+from PyQt5.QtMultimedia import QMediaPlaylist, QMediaPlayer, QMediaContent
 
 
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.player = QMediaPlayer()
+        self.playlist = QMediaPlaylist()
+        self.menubar = QMenuBar()
         self.title = 'PyQt5 music player'
         self.left = 10
         self.top = 10
@@ -14,18 +19,33 @@ class App(QWidget):
         self.initUI()
 
     def initUI(self):
-        button = QPushButton("Button", self)
-        button.setToolTip("Example button")
-        button.move(100, 70)
-        button.clicked.connect(self.onClick)
+        # Add file menu
+        filemenu = self.menubar.addMenu('File')
+        filemenu.addAction(self.fileOpen())
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
 
-    def onClick(self):
-        print("Button clicked!")
-        dir_ = QFileDialog.getExistingDirectory(None, 'Select a folder:', '/', QFileDialog.ShowDirsOnly)
-        print(dir_)
+    def fileOpen(self):
+        fileAc = QAction('Open File',self)
+        fileAc.setShortcut('Ctrl+O')
+        fileAc.setStatusTip('Open File')
+        fileAc.triggered.connect(self.openFile)
+        return fileAc
+
+    def openFile(self):
+        print("File button clicked!")
+        song = QFileDialog.getOpenFileName(self, "Open Song", "~/", "Sound Files (*.mp3)")
+        print(song[0])
+        
+        url = QUrl.fromLocalFile(song[0])
+        self.playlist.addMedia(QMediaContent(url))
+        self.playlist.setPlaybackMode(QMediaPlaylist.Loop)
+
+        self.player.setPlaylist(self.playlist)
+        self.player.playlist().setCurrentIndex(0)
+        self.player.play()
 
 
 if __name__ == '__main__':
