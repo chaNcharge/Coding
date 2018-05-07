400f3181c396bcad6a7c01cf6bd30422160b955a
chaNcharge <chancharge@gmail.com>
2018-05-07 13:05:35 -0700

    Add function to calculate expected numbers

--- chisquare calculator.py
+++ chisquare calculator.py
@@ -2,6 +2,8 @@ from scipy.stats import chisquare
 
 o = []
 e = []
+ratio = []
+recommendedexpected = []
 
 while True:
     tocalc = input("Enter one observed number. Enter a blank to finish. ")
@@ -9,6 +11,20 @@ while True:
         break
     else:
         o.append(float(tocalc))
+
+while True:
+    toratio = input("Enter hypothesis ratio, one number at a time. Enter a blank to finish. ")
+    if toratio == "":
+        break
+    else:
+        ratio.append(int(toratio))
+
+ratiototal = sum(ratio)
+observedtotal = sum(o)
+for n in ratio:
+    torecommendedexpected = observedtotal / ratiototal * n
+    recommendedexpected.append(round(torecommendedexpected, 2))
+print("Recommended expected numbers: ", recommendedexpected)
 
 while True:
     toexp = input("Enter one expected number. Enter a blank to finish. ")
